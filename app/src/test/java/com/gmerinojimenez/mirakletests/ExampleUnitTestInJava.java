dd6b300671e52e847d3c54f197a7aac0c2fe9396
gmerino <guillermo.merino.jimenez@telefonica.com>
2022-02-03 09:58:28 +0100

    Added a test written in Java

---
+++ app/src/test/java/com/gmerinojimenez/mirakletests/ExampleUnitTestInJava.java
@@ -0,0 +1,13 @@
+package com.gmerinojimenez.mirakletests;
+
+import static junit.framework.TestCase.assertEquals;
+
+import org.junit.Test;
+
+public class ExampleUnitTestInJava {
+
+    @Test
+    public void twoPlusTwoIsFiveInJava() {
+        assertEquals(5, 2 + 2);
+    }
+}
